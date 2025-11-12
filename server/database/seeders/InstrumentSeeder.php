7cfe489f58c0d9e803246b001b3eb4913cb832f7
MolnarKristof0719 <csangcsungcsing@gmail.com>
2025-11-12 14:50:37 +0100

    seeder

--- server/database/seeders/InstrumentSeeder.php
+++ server/database/seeders/InstrumentSeeder.php
@@ -12,6 +12,29 @@ class InstrumentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [];
+
+        $filePath = database_path('csv/products.csv');
+        $data = [];
+        $header = []; // Fejlécek tárolására
+ 
+        if (($handle = fopen($filePath, 'r')) !== false) {
+            // 1. Beolvassuk a fejléceket (ha vannak)
+            $header = fgetcsv($handle, 0, ';');
+ 
+            // 2. Soronként beolvassuk az adatokat (0 azt jelenti, hogy nincs korlát a beolvasott sorra)
+            while (($cols = fgetcsv($handle, 0, ';')) !== false) {
+                if (count($header) === count($cols)) {
+                    // Asszociatív tömb létrehozása (jobb olvashatóság!)
+                    $data[] = array_combine($header, $cols);
+                }
+            }
+            // 3. Zárjuk a fájlt (itt kötelező!)
+            fclose($handle);
+        }
+
+        if (Product::count() === 0) {
+            Product::factory()->createMany($data);
+        }
     }
 }
